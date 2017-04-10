f940558332224f7323a2de204677249c52a8e884
odykyi <odykyi@luxoft.com>
2017-04-10 03:21:34 +0300

    transformResponse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-
-import VueRouter from 'vue-router';
-import auth from './auth';
 import axios from 'axios';
+import VueRouter from 'vue-router';
+
+import auth from './auth';
 import {
   App,
   Home,
